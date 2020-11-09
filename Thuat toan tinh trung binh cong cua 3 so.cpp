d91bfb2b3b85d04d34c36957339ad55baf91c522
phamhongcanh245 <71203206+phamhongcanh245@users.noreply.github.com>
2020-11-09 08:09:37 +0700

    Delete Thuat toan tinh trung binh cong cua 3 so.cpp

--- Thuat toan tinh trung binh cong cua 3 so.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	float a, b, c;
-	cout<<"Nhap lan luot 3 so: "; cin>>a>>b>>c;
-	cout<<"Trung binh cong cua 3 so tren la: "<<(a+b+c)/3;
-	return 0;
-}
